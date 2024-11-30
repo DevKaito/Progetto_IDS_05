a09f3d22f3200822f3c083370dd3067a4ab90619
LiLDak777 <m.dacunto18@studenti.unisa.it>
2024-11-30 17:23:02 +0100

    Update OrdinaContatto.java
    Aggiunta commenti Doxygen all'interfaccia OrdinaContatto.

--- src/main/java/progetto_ids_05/Interfaces/OrdinaContatto.java
+++ src/main/java/progetto_ids_05/Interfaces/OrdinaContatto.java
@@ -2,6 +2,14 @@ package progetto_ids_05.Interfaces;
 
 import progetto_ids_05.Ordinamento;
 
+/**
+* @brief Interfaccia per l'implementazione del metodo che si occupa dell'ordinamento dei contatti.
+*/
 public interface OrdinaContatto {
+    /**
+    * @brief Applica l'ordinamento in base a "Ordinamento o" passato da parametro.
+    * 
+    * @param o Definisce il tipo di ordinamento.
+    */
     public void applicaOrdinamento(Ordinamento o);
 }
