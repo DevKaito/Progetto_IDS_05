1798020109e205c4aeda188cf77e6e029c2d1798
LiLDak777 <m.dacunto18@studenti.unisa.it>
2024-11-30 17:06:24 +0100

    Update Rubrica.java
    Aggiunta dei commenti dei metodi seguenti: Rubrica, aggiungiContattoFile, eseguiRicerca, applicaOrdinamento.

--- src/main/java/progetto_ids_05/Rubrica.java
+++ src/main/java/progetto_ids_05/Rubrica.java
@@ -7,6 +7,11 @@ import java.util.TreeMap;
 public class Rubrica implements CercaContatto, OrdinaContatto {
     private Map<Integer, Contatto> contatti;
     
+    /**
+    * @brief Inizializza la struttura dati "contatti".
+    * 
+    * Inizializza una struttura dati di tipo TreeMap<>().
+    */
     public Rubrica(){
         contatti = new TreeMap<>();
     }
@@ -14,7 +19,7 @@ public class Rubrica implements CercaContatto, OrdinaContatto {
     /**
     * @brief Crea e aggiunge un contatto alla rubrica.
     *
-    * Prende come parametro un contatto "Contatto c" da aggiungere alla rubrica e lo aggiunge.
+    * Prende come parametro un contatto "Contatto c" da aggiungere alla rubrica.
     *
     * @pre none
     * @post Il contatto viene aggiunto alla rubrica.
@@ -22,11 +27,19 @@ public class Rubrica implements CercaContatto, OrdinaContatto {
     * @param c Il contatto "Contatto c" da aggiungere alla rubrica.
     */
     public void aggiungiContattoRubrica(Contatto c){
-        //aaaa
-    }
         
-     //-
-    
+    }
+
+    /**
+    * @brief Aggiunge un contatto in rubrica.
+    * 
+    * Prende come parametro un contatto "Contatto c" da aggiungere alla rubrica da un file esterno.
+    * 
+    * @pre Venga scelto almeno un contatto e il file abbia almeno un contatto.
+    * @post I contatti scelti dal file vengono aggiunti alla rubrica.
+    * 
+    * @param c Il contatto "Contatto c" da aggiungere alla rubrica.
+    */
     public void aggiungiContattoFile(Contatto c){
         TrasferimentoContatti.importaContatto(contatti, "contatti.ser");
     }
@@ -61,12 +74,33 @@ public class Rubrica implements CercaContatto, OrdinaContatto {
         
     }
     
+    /**
+    * @brief Ricerca un contatto.
+    * 
+    * Prende come parametro una String searchValue e ritorna un array "Contatto[]", filtrato in base alla stringa presa da parametro.
+    * 
+    * @pre Presenza di contatti in rubrica.
+    * @post La rubrica viene filtrata in base al testo scritto nella barra di ricerca.
+    * 
+    * @param searchValue La stringa di testo inserita nella barra di ricerca.
+    * @return Contatto[].
+    */
     @Override
     public Contatto[] eseguiRicerca(String searchValue){
         return new Contatto[1];
     }
     
-   @Override
+    /**
+    * @brief Applica un ordinamento.
+    * 
+    * Prende in input "Ordinamento o" e ordina la rubrica in base all'ordinamento che viene passato da parametro.
+    * 
+    * @pre Presenza di contatti in rubrica.
+    * @post I contatti in rubrica vengono visualizzati nell'ordine corrispondente al tipo di ordinamento passato da parametro.
+    * 
+    * @param o Tipo di ordinamento selezionato. 
+    */
+    @Override
     public void applicaOrdinamento(Ordinamento o){
         
     }
