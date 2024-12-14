a85d127f128f9a84987fbec93a78187507abc18e
Franzab <francesco.abballe04@gmail.com>
2024-12-14 13:33:45 +0100

    AddEdit

---
+++ src/main/java/controller/AddEditContactController.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import classes.Contatto;
+import classes.Rubrica;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author franc
+ */
+public class AddEditContactController implements Initializable {
+
+    @FXML
+    private TextField nameFld;
+    @FXML
+    private TextField surnameFld;
+    @FXML
+    private TextField numberFld1;
+    @FXML
+    private TextField numberFld2;
+    @FXML
+    private TextField numberFld3;
+    @FXML
+    private TextField emailFld1;
+    @FXML
+    private TextField emailFld2;
+    @FXML
+    private TextField emailFld3;
+    @FXML
+    private Button submitBtn;
+    
+    private Rubrica rubrica;
+    private Contatto contatto;
+    private Boolean isEditMode = false;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (isEditMode && contatto != null) {
+            // Popola i campi con i dati del contatto esistente
+            nameFld.setText(contatto.getNome());
+            surnameFld.setText(contatto.getCognome());
+
+            String[] numeri = contatto.getNumeriTelefono();
+            if (numeri.length > 0) numberFld1.setText(numeri[0]);
+            if (numeri.length > 1) numberFld2.setText(numeri[1]);
+            if (numeri.length > 2) numberFld3.setText(numeri[2]);
+
+            String[] emails = contatto.getIndirizziEmail();
+            if (emails.length > 0) emailFld1.setText(emails[0]);
+            if (emails.length > 1) emailFld2.setText(emails[1]);
+            if (emails.length > 2) emailFld3.setText(emails[2]);
+        }
+    }
+    
+    public void setEditMode(Boolean isEditMode){
+        this.isEditMode = isEditMode;
+    }
+    
+    public void setRubrica(Rubrica rubrica){
+        this.rubrica = rubrica;
+    }
+    
+    public void setContatto(Contatto contatto){
+        this.contatto = contatto;
+        
+    }
+    
+    @FXML
+    private void handleSubmit(){
+        String nome = nameFld.getText();
+        String cognome = surnameFld.getText();
+        String[] numeriTelefono = {
+            numberFld1.getText(),
+            numberFld2.getText(),
+            numberFld3.getText(),
+        };
+        String[] indirizziEmail = {
+            emailFld1.getText(),
+            emailFld2.getText(),
+            emailFld3.getText(),
+        };
+        
+        if (isEditMode && contatto != null){
+            rubrica.modificaContatto(nome, cognome, numeriTelefono, indirizziEmail, contatto.getID());
+        }else rubrica.aggiungiContattoRubrica(nome, cognome, numeriTelefono, indirizziEmail);
+        
+        Stage stage = (Stage) submitBtn.getScene().getWindow();
+        stage.close();
+    } 
+
+    
+
+    
+    
+}
--- src/main/java/controller/RubricaViewController.java
+++ src/main/java/controller/RubricaViewController.java
@@ -2,15 +2,26 @@ package controller;
 
 import classes.Contatto;
 import classes.Rubrica;
+import enumerators.Ordinamento;
+import java.io.IOException;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class RubricaViewController {
     private Rubrica rubrica;    
@@ -28,6 +39,15 @@ public class RubricaViewController {
     private TableColumn<Contatto, String> numeriDiTelefonoColumn;
     @FXML
     private TableColumn<Contatto, String> indirizziEmailColumn;
+    @FXML
+    private TableColumn<Contatto, Void> gestioneColumn;
+    @FXML
+    private Button addBtn;
+    @FXML
+    private Button importBtn;
+    @FXML
+    private Button exportBtn;
+    
     
     @FXML
     public void initialize(){
@@ -38,12 +58,21 @@ public class RubricaViewController {
         //Combobox initialization
         cmb.getItems().addAll("Per aggiunta", "Alfabetico");
         cmb.getSelectionModel().selectFirst();
+        
+        cmb.setOnAction(event -> handleOrdinamento());
+        
+        gestisciButtons();
+        
+        addBtn.setOnAction(event -> addContactWindow());
+    
+
        
         //TableView initialization
         rubrica = new Rubrica();
         rubrica.aggiungiContattoRubrica("si", "no", new String[]{"3341847499"}, new String[]{"mario@gmail.com"});
         rubrica.aggiungiContattoRubrica("no", "si", new String[]{"3341847498", "3341857499"}, new String[]{"nintendo@gmail.com"});
-        rubrica.aggiungiContattoRubrica("sto", "cazzo", new String[]{"3761847499"}, new String[]{"ophelia@dnd.com"});
+        rubrica.aggiungiContattoRubrica("Amedeo", "Pratola", new String[]{"3761847499"}, new String[]{"ophelia@dnd.com"});
+        rubrica.aggiungiContattoRubrica("Amedeo", "Bianco", new String[]{"3761347499", "9898908983"}, new String[]{"violayio@dnd.com"});
         ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
         
         nomeColumn.setCellValueFactory(new PropertyValueFactory<Contatto, String>("nome"));
@@ -59,9 +88,110 @@ public class RubricaViewController {
             return new SimpleStringProperty(indirizziUniti);
         });
         
+        
+        
         tableView.setItems(data);
     }
     
+        private void handleOrdinamento(){
     
+            String opzioneSelezionata = (String) cmb.getValue();
+            
+                if ("Alfabetico".equals(opzioneSelezionata)){
+                    rubrica.applicaOrdinamento(Ordinamento.OrdineAlfabetico);
+                } else if ("Per aggiunta".equals(opzioneSelezionata)){
+                    rubrica.applicaOrdinamento(Ordinamento.OrdineStandard);
+                }
+            ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+            tableView.setItems(data);
+        }
     
+        private void addContactWindow() {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AddEditContact.fxml"));
+                Parent root = loader.load();
+                AddEditContactController controller = loader.getController();
+                controller.setRubrica(rubrica);
+
+                Stage stage = new Stage();
+                stage.setTitle("Aggiungi Contatto");
+                stage.setScene(new Scene(root));
+                stage.showAndWait();
+
+                ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+                tableView.setItems(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+        }
 }
+    
+        private void gestisciButtons() {
+            gestioneColumn.setCellFactory(param -> new TableCell<Contatto, Void>() {
+            private final HBox container = new HBox();
+            private final Button modificaButton = new Button("Modifica");
+            private final Button rimuoviButton = new Button("Rimuovi");
+        
+
+            {     
+                modificaButton.setOnAction(event -> {
+                    Contatto contatto = getTableView().getItems().get(getIndex());
+                    if (contatto != null) {
+                        modifyContactWindow(contatto);
+                    } else {
+                        System.err.println("Contatto non valido per modifica.");
+                    }
+                });
+
+                rimuoviButton.setOnAction(event -> {
+                    Contatto contatto = getTableView().getItems().get(getIndex());
+                    if (contatto != null) {
+                        rubrica.rimuoviContatto(contatto.getID());
+                        ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+                        tableView.setItems(data);
+                    } 
+                });
+
+                modificaButton.setStyle("-fx-background-color: lightblue; -fx-text-fill: black;");
+                rimuoviButton.setStyle("-fx-background-color: lightcoral; -fx-text-fill: black;");
+                container.setSpacing(10); // Spazio tra i pulsanti
+                container.getChildren().addAll(modificaButton, rimuoviButton);
+            }
+        
+        
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null); 
+                } else {
+                    setGraphic(container); 
+                }
+            }
+            });
+        }
+        
+        private void modifyContactWindow(Contatto c){
+            try{
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/AddEditContact.fxml"));
+                Parent root = loader.load();
+                AddEditContactController controller = loader.getController();
+                controller.setRubrica(rubrica);
+                controller.setContatto(c);
+                controller.setEditMode(true);
+                
+                Stage stage = new Stage();
+                stage.setTitle("Modifica contatto");
+                stage.setScene(new Scene(root));
+                stage.showAndWait();
+                ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+                tableView.setItems(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+        }
+            
+}
+        
+        
+}
+
