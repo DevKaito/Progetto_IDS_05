1b4ca744bc4a92d3de17a5335c083f1438fda82c
MDL04 <m.dilieto12@studenti.unisa.it>
2024-12-14 23:44:32 +0100

    JavaFX export/import

---
+++ src/main/java/controller/Import_ExportFileController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import javafx.scene.control.Button;
+/**
+ *
+ * @author User
+ */
+public class Import_ExportFileController{
+    private RubricaViewController mainController;
+    private boolean isExport = false;
+    @FXML
+    private Button bimportID;
+
+    public void initialize(){
+        bimportID.setOnAction(event -> handleFileSelection());
+    }
+    
+    public void setMainController(RubricaViewController mainController) {
+        this.mainController = mainController;
+    }
+    
+    public void handleFileSelection(){
+        Stage stage = (Stage) bimportID.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+        
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null && !isExport) {
+            mainController.importDataFromFile(selectedFile);
+            stage.close(); 
+        }
+        else if (selectedFile != null && isExport){
+            mainController.ExportDataToFile(selectedFile);
+            stage.close();
+        }
+        
+    }
+    
+    public void setMode(Boolean bo){
+        this.isExport = bo;
+    }
+    
+}
--- src/main/java/controller/RubricaViewController.java
+++ src/main/java/controller/RubricaViewController.java
@@ -3,7 +3,10 @@ package controller;
 import classes.Contatto;
 import classes.Rubrica;
 import enumerators.Ordinamento;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,6 +14,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableCell;
@@ -18,8 +23,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -64,6 +67,10 @@ public class RubricaViewController {
         //Utility buttons inizialization
         gestisciButtons();
         addBtn.setOnAction(event -> addContactWindow());
+        
+        //Import & Export
+        importBtn.setOnAction(event -> ImportContactWindow());
+        exportBtn.setOnAction(event -> ExportContactWindow());
        
         //TableView initialization con contatti di prova
         rubrica = new Rubrica();
@@ -72,105 +79,107 @@ public class RubricaViewController {
         rubrica.aggiungiContattoRubrica("Amedeo", "Pratola", new String[]{"3761847499"}, new String[]{"ophelia@dnd.com"});
         rubrica.aggiungiContattoRubrica("Amedeo", "Bianco", new String[]{"3761347499", "9898908983"}, new String[]{"violayio@dnd.com"});
         
+        BindingRicerca();
+        
         updateTableView();
     }
     private void updateTableView(){
-            ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+        ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
         
-            //Columns initialization
-            nomeColumn.setCellValueFactory(new PropertyValueFactory<Contatto, String>("nome"));
-            cognomeColumn.setCellValueFactory(new PropertyValueFactory<Contatto, String>("cognome"));
-            numeriDiTelefonoColumn.setCellValueFactory(cellData -> {
-                String[] numeri = cellData.getValue().getNumeriTelefono();
-                String numeriUniti = String.join("\n", numeri);
-                return new SimpleStringProperty(numeriUniti);
-            });
-            indirizziEmailColumn.setCellValueFactory(cellData -> {
-                String[] indirizzi = cellData.getValue().getIndirizziEmail();
-                String indirizziUniti = String.join("", indirizzi);
-                return new SimpleStringProperty(indirizziUniti);
-            });
+        //Columns initialization
+        nomeColumn.setCellValueFactory(new PropertyValueFactory<Contatto, String>("nome"));
+        cognomeColumn.setCellValueFactory(new PropertyValueFactory<Contatto, String>("cognome"));
+        numeriDiTelefonoColumn.setCellValueFactory(cellData -> {
+            String[] numeri = cellData.getValue().getNumeriTelefono();
+            String numeriUniti = String.join("\n", numeri);
+            return new SimpleStringProperty(numeriUniti);
+        });
+        indirizziEmailColumn.setCellValueFactory(cellData -> {
+            String[] indirizzi = cellData.getValue().getIndirizziEmail();
+            String indirizziUniti = String.join("\n", indirizzi);
+            return new SimpleStringProperty(indirizziUniti);
+        });
         
-            tableView.setItems(data);
-            tableView.refresh();
-        }
+        tableView.setItems(data);
+        tableView.refresh();
+    }
     
     private void handleOrdinamento(){
-    
-            String opzioneSelezionata = (String) cmb.getValue();
+        String opzioneSelezionata = (String) cmb.getValue();
             
-                if ("Alfabetico".equals(opzioneSelezionata)){
-                    rubrica.applicaOrdinamento(Ordinamento.OrdineAlfabetico);
-                } else if ("Per aggiunta".equals(opzioneSelezionata)){
-                    rubrica.applicaOrdinamento(Ordinamento.OrdineStandard);
-                }
-            ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
-            tableView.setItems(data);
+        if ("Alfabetico".equals(opzioneSelezionata)){
+           rubrica.applicaOrdinamento(Ordinamento.OrdineAlfabetico);
+        } else if ("Per aggiunta".equals(opzioneSelezionata)){
+           rubrica.applicaOrdinamento(Ordinamento.OrdineStandard);
         }
+        ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+        tableView.setItems(data);
+    }
     
     private void gestisciButtons() {
-            gestioneColumn.setCellFactory(param -> new TableCell<Contatto, Void>() {
-            private final HBox container = new HBox();
-            private final Button modificaButton = new Button("Modifica");
-            private final Button rimuoviButton = new Button("Rimuovi");
+        gestioneColumn.setCellFactory(param -> new TableCell<Contatto, Void>() {
+        private final HBox container = new HBox();
+        private final Button modificaButton = new Button("Modifica");
+        private final Button rimuoviButton = new Button("Rimuovi");
             
-            {     
-                modificaButton.setOnAction(event -> {
-                    Integer ID = getTableView().getItems().get(getIndex()).getID();
-                    if (ID != null) {
-                        modifyContactWindow(ID);
-                        ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
-                        tableView.setItems(data);
-                    } else {
-                        System.err.println("Contatto non valido per modifica.");
-                    }
-                });
+        {    
+            modificaButton.setOnAction(event -> {
+            Integer ID = getTableView().getItems().get(getIndex()).getID();
+            if (ID != null) {
+                modifyContactWindow(ID);
+                ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+                tableView.setItems(data);
+            } else {
+                System.err.println("Contatto non valido per modifica.");
+            }
+        });
 
-                {rimuoviButton.setOnAction(event -> {
-                    Contatto contatto = getTableView().getItems().get(getIndex());
-                    if (contatto != null) {
-                        rubrica.rimuoviContatto(contatto.getID());
-                        ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
-                        tableView.setItems(data);
-                    } 
-                });
+            {rimuoviButton.setOnAction(event -> {
+                Contatto contatto = getTableView().getItems().get(getIndex());
+                if (contatto != null) {
+                    rubrica.rimuoviContatto(contatto.getID());
+                    ObservableList<Contatto> data = FXCollections.observableArrayList(rubrica.getContatti().values());
+                    tableView.setItems(data);
+                    tableView.refresh();
                 }
-                modificaButton.setStyle("-fx-background-color: lightblue; -fx-text-fill: black;");
-                rimuoviButton.setStyle("-fx-background-color: lightcoral; -fx-text-fill: black;");
-                container.setSpacing(10); // Spazio tra i pulsanti
-                container.getChildren().addAll(modificaButton, rimuoviButton);
-            }
-    
-            @Override
-            protected void updateItem(Void item, boolean empty) {
-                super.updateItem(item, empty);
-                if (empty) {
-                    setGraphic(null); 
-                } else {
-                    setGraphic(container); 
-                }
-            }
             });
+            }
+            modificaButton.setStyle("-fx-background-color: lightblue; -fx-text-fill: black;");
+            rimuoviButton.setStyle("-fx-background-color: lightcoral; -fx-text-fill: black;");
+            container.setSpacing(10); // Spazio tra i pulsanti
+            container.getChildren().addAll(modificaButton, rimuoviButton);
         }
+    
+        @Override
+        protected void updateItem(Void item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty) {
+                setGraphic(null); 
+            } else {
+                setGraphic(container); 
+            }
+        }
+        });
+    }
         
     private void addContactWindow() {
-            try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/AddEditContact.fxml"));
-                Parent root = loader.load();
-                AddEditContactController controller = loader.getController();
-                controller.setRubrica(rubrica);
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/AddEditContact.fxml"));
+            Parent root = loader.load();
+            AddEditContactController controller = loader.getController();
+            controller.setRubrica(rubrica);
 
-                Stage stage = new Stage();
-                stage.setTitle("Aggiungi Contatto");
-                stage.setScene(new Scene(root));
-                stage.initModality(Modality.APPLICATION_MODAL);
-                stage.showAndWait();
+            Stage stage = new Stage();
+            stage.setTitle("Aggiungi Contatto");
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
 
-                updateTableView();
+            updateTableView();
             } catch (IOException e) {
                 e.printStackTrace();
-        }
-}
+            }
+    }
         
     private void modifyContactWindow(Integer ID){
             try{
@@ -195,7 +204,80 @@ public class RubricaViewController {
                 e.printStackTrace();
             }
             
-}
+    }   
+    private void ImportContactWindow(){
+        try{
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/Import_Export.fxml"));
+                Parent root = loader.load();
+                Import_ExportFileController importaController = loader.getController();
+                importaController.setMainController(this);
+                Stage stage = new Stage();
+                stage.setTitle("Importa Contatti");
+                stage.setScene(new Scene(root));
+                stage.showAndWait();
+                
+        } catch (IOException e){}
+            
+    }   
+    public void importDataFromFile(File file) {
+        rubrica.aggiungiContattoFile(file.getAbsolutePath());
         
-}        
+        ObservableList<Contatto> contattiList = FXCollections.observableArrayList(rubrica.getContatti().values());
+        tableView.setItems(contattiList);
 
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Importazione Contatti");
+        alert.setHeaderText("Contatti importati");
+        alert.showAndWait();
+    }
+    
+    private void ExportContactWindow(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/Import_Export.fxml"));
+            Parent root = loader.load();
+            Import_ExportFileController esportaController = loader.getController();
+            esportaController.setMode(true);
+            esportaController.setMainController(this);
+            Stage stage = new Stage();
+            stage.setTitle("Export Contatti");
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+                
+        } catch (IOException e){}
+        
+    }
+    
+    public void ExportDataToFile(File file) {
+        rubrica.esportaRubricaFile(file.getAbsolutePath());
+        
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Esportazione Contatti");
+        alert.setHeaderText("Contatti esportati");
+        alert.showAndWait();
+    }
+    
+    public void BindingRicerca(){
+        searchBar.textProperty().addListener((observable, oldValue, newValue) ->{
+                String[] parole = newValue.trim().split("\\s+");
+                if (parole.length==0 || parole[0].isEmpty()){
+                    tableView.setItems(FXCollections.observableArrayList(rubrica.getContatti().values()));
+                    return;
+                }
+                
+                List<Contatto> risultati = rubrica.getContatti().values().stream().filter(contatto -> {
+                    for (String parola : parole){
+                        if (!contatto.getNome().toLowerCase().contains(parola.toLowerCase()) && !contatto.getCognome().toLowerCase().contains(parola.toLowerCase()))
+                            return false;
+                    }
+                    return true;
+                })
+                .collect(Collectors.toList());
+                
+                tableView.setItems(FXCollections.observableArrayList(risultati));
+        });
+        
+        
+    }
+
+    
+}
